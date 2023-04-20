4c209eec74c6eaf25a5f3230a3ab2de007aa0cdd
Shamslux <jpmamed@gmail.com>
2023-04-20 10:01:59 -0300

    (new): Exercise 20 solution! Stored procedures basics!

---
+++ Codes/E20 - STORED PROCEDURES BASICS.sql
@@ -0,0 +1,28 @@
+/*
+Create a stored procedure called "OrdersAboveThreshold" that pulls in all sales orders with a total due amount above a 
+threshold specified in a parameter called "@Threshold". The value for threshold will be supplied by the caller of the stored procedure.
+The proc should have two other parameters: "@StartYear" and "@EndYear" (both INT data types), also specified by the called of the procedure. 
+All order dates returned by the proc should fall between these two years.
+
+*/
+
+CREATE PROCEDURE dbo.OrdersAboveThreshold(@Threshold MONEY, @StartYear INT, @EndYear INT)
+
+AS
+
+BEGIN
+	SELECT	SOH.SalesOrderID
+			, SOH.OrderDate
+			, SOH.TotalDue
+	FROM  AdventureWorks2019.Sales.SalesOrderHeader	SOH
+	INNER JOIN AdventureWorks2019.dbo.Calendar		C
+	ON SOH.OrderDate = C.DATE_VALUE 
+	WHERE SOH.TotalDue >= @Threshold
+		AND C.YEAR_NUMBER BETWEEN @StartYear AND @EndYear
+END
+--------------------------------------------------------
+
+EXEC dbo.OrdersAboveThreshold 10000, 2011, 2013
+
+
+
